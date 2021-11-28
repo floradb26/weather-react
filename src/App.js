326294d75c2cd77e1ef4a27986f1a15f5da239ae
floradb26 <debruyne.f@outlook.com>
2021-11-28 18:05:19 +0100

    Page presentation h1

--- src/App.js
+++ src/App.js
@@ -11,14 +11,6 @@ function App() {
         Hello World!
         </h1>
         <Weather city="Brussels"/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
