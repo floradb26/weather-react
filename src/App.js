5bef7bb66d7b711b3d219b84ec5d694e40d2b394
floradb26 <debruyne.f@outlook.com>
2021-11-28 19:42:10 +0100

    addition of font awesome

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
   return (
     <div className="App">
       <div className="card">
+        <div className="cardHeader">
           <Weather city="Brussels"/>
+        </div>
       </div>
     </div>
   );
